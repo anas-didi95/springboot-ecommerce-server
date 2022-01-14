c352a1800dc606ee16160a0e85e9121f614470c6
Anas Juwaidi <anas.didi95@gmail.com>
2022-01-14 16:17:30 +0800

    ADD: Add sample controller

---
+++ src/test/java/com/anasdidi/ecommerce/service/producttype/ProductTypeControllerTests.java
@@ -0,0 +1,28 @@
+package com.anasdidi.ecommerce.service.producttype;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+class ProductTypeControllerTests {
+
+  private final WebTestClient webTestClient;
+
+  @Autowired
+  ProductTypeControllerTests(WebTestClient webTestClient) {
+    this.webTestClient = webTestClient;
+  }
+
+  @Test
+  void testProductTypeCreateSuccess() {
+    webTestClient.get().uri("/producttype").accept(MediaType.APPLICATION_JSON).exchange().expectStatus()
+        .isEqualTo(HttpStatus.OK);
+  }
+}
